2ebbd27c1585282baa63e8bed9875b573a1c900d
CS12300 <108864806+CS12300@users.noreply.github.com>
2022-07-08 18:33:11 +0800

    Create fyi.cpp

---
+++ fyi.cpp
@@ -0,0 +1,22 @@
+//
+FYI
+https://open.kattis.com/problems/fyi
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        int tel;
+        cin >> tel;
+
+        if (tel >= 5550000 && tel < 5560000)
+          cout << 1;
+        else
+          cout << 0;
+
+     return 0;
+}
+
