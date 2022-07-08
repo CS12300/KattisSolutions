97f76e54fc2449f2cb03d5fd06f9cb4f20feb6ea
CS12300 <108864806+CS12300@users.noreply.github.com>
2022-07-08 18:32:11 +0800

    Update gcvwr.cpp

--- gcvwr.cpp
+++ gcvwr.cpp
@@ -9,20 +9,20 @@ using namespace std;
 
 int main()
 {
-  int g, t, n;
-  int sum = 0;
-  
-  cin >> g >> t >> n;
-  
-  for (int i = 0; i < n; i++)
-    {
-      int weight = 0;
-      
-      cin >> weight;
-      sum += weight;
-    }
-  cout << (g-t)*0.9 - sum;
+        int g, t, n;
+        int sum = 0;
 
-return 0;  
+        cin >> g >> t >> n;
+
+        for (int i = 0; i < n; i++)
+          {
+            int weight = 0;
+
+            cin >> weight;
+            sum += weight;
+          }
+        cout << (g-t)*0.9 - sum;
+
+    return 0;  
 }
 
