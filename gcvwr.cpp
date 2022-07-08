1c82f3f53d63de49f7f6b2078b4955684a80af80
CS12300 <108864806+CS12300@users.noreply.github.com>
2022-07-08 18:06:48 +0800

    Create gcvwr.cpp

---
+++ gcvwr.cpp
@@ -0,0 +1,28 @@
+//
+GCVWR
+https://open.kattis.com/problems/gcvwr
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int g, t, n;
+  int sum = 0;
+  
+  cin >> g >> t >> n;
+  
+  for (int i = 0; i < n; i++)
+    {
+      int weight = 0;
+      
+      cin >> weight;
+      sum += weight;
+    }
+  cout << (g-t)*0.9 - sum;
+
+return 0;  
+}
+
